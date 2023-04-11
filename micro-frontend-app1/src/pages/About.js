f7120847b9811fcf85ed4ab2583046a6e19c684e
黄娟 <huang.juan@99cloud.net>
2023-04-11 10:12:07 +0800

    add app1 router

---
+++ micro-frontend-app1/src/pages/About.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+      <div>
+        this is app1 about page
+      </div>
+    );
+  }
+
+  export default Home;
---
+++ micro-frontend-app1/src/pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      this is app1 home page
+    </div>
+  );
+}
+
+export default Home;
